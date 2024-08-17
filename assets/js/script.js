b84573cd0944696f008f9e9d9b5c513970e085bb
OmarRwiheb <omarrwiheb99@gmail.com>
2024-08-17 16:45:39 +0200

    beta version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,4 @@
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
 
-navBarToggle.addEventListener("click", function() {
-  mainNav.classList.toggle("active");
-});
 document.addEventListener("DOMContentLoaded", function() {
   const navbar = document.querySelector('.navbar');
   
@@ -15,3 +10,29 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 });
+
+let mobNav = document.querySelector('#dropdown-head-mob');
+let mobNav_menu = document.getElementById('mob-dropdown');
+let cover = document.querySelector('.list-cover');
+mobNav.addEventListener('click', () => {
+  if(cover.style.transform == "translateY(1px)")cover.style.transform = "translateY(110px)"
+  else cover.style.transform = "translateY(1px)"
+  
+})
+
+let hamburger = document.querySelector('.hamburger');
+let mob = document.querySelector('.mobile-nav')
+hamburger.addEventListener('click', () => {
+  if(mob.style.display == "none"){
+    mob.style.display = "block"; 
+    setTimeout(() => {document.querySelector('.nav-content').style.transform= "translateX(100%)";}, 100)
+  }
+  else {
+    setTimeout(() => {document.querySelector('.nav-content').style.transform= "translateX(-100%)";}, 100)
+    mob.style.display = "none";
+  }
+})
+document.querySelector('.nav-blocker').addEventListener('click', () => {
+  setTimeout(() => {document.querySelector('.nav-content').style.transform= "translateX(-100%)";}, 100)
+  mob.style.display = "none"
+})
